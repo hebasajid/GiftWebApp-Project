b679f69e5076e6297aefac1dbf048f47d185508d
Heba Sajid <hsajid35467@gmail.com>
2023-11-30 20:32:53 -0500

    2nd controller added. db context error fixed

---
+++ GiftAPI/Controllers/ParentGiftsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GiftInfoLibraryy.Models;
+
+namespace GiftAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParentGiftsController : ControllerBase
+    {
+        private readonly GiftInfoDbContext _context;
+
+        public ParentGiftsController(GiftInfoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ParentGifts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ParentGift>>> GetParentGifts()
+        {
+          if (_context.ParentGifts == null)
+          {
+              return NotFound();
+          }
+            return await _context.ParentGifts.ToListAsync();
+        }
+
+        // GET: api/ParentGifts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ParentGift>> GetParentGift(int id)
+        {
+          if (_context.ParentGifts == null)
+          {
+              return NotFound();
+          }
+            var parentGift = await _context.ParentGifts.FindAsync(id);
+
+            if (parentGift == null)
+            {
+                return NotFound();
+            }
+
+            return parentGift;
+        }
+
+        // PUT: api/ParentGifts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutParentGift(int id, ParentGift parentGift)
+        {
+            if (id != parentGift.PGiftId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(parentGift).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ParentGiftExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ParentGifts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ParentGift>> PostParentGift(ParentGift parentGift)
+        {
+          if (_context.ParentGifts == null)
+          {
+              return Problem("Entity set 'GiftInfoDbContext.ParentGifts'  is null.");
+          }
+            _context.ParentGifts.Add(parentGift);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetParentGift", new { id = parentGift.PGiftId }, parentGift);
+        }
+
+        // DELETE: api/ParentGifts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteParentGift(int id)
+        {
+            if (_context.ParentGifts == null)
+            {
+                return NotFound();
+            }
+            var parentGift = await _context.ParentGifts.FindAsync(id);
+            if (parentGift == null)
+            {
+                return NotFound();
+            }
+
+            _context.ParentGifts.Remove(parentGift);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ParentGiftExists(int id)
+        {
+            return (_context.ParentGifts?.Any(e => e.PGiftId == id)).GetValueOrDefault();
+        }
+    }
+}
--- GiftInfoLibraryy/Models/GiftInfoDbContext.cs
+++ GiftInfoLibraryy/Models/GiftInfoDbContext.cs
@@ -21,9 +21,9 @@ public partial class GiftInfoDbContext : DbContext
 
     public virtual DbSet<UserFavoriteGift> UserFavoriteGifts { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=giftapi-sql.ci56eydvwqo6.ca-central-1.rds.amazonaws.com,1433;database=GiftInfoDB;TrustServerCertificate=True;User ID=Heba; Password=password123;");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=giftapi-sql.ci56eydvwqo6.ca-central-1.rds.amazonaws.com,1433;database=GiftInfoDB;TrustServerCertificate=True;User ID=Heba; Password=password123;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
