4d290f2bf6d0c2e0d7a84053a76cd92390668060
Heba Sajid <hsajid35467@gmail.com>
2023-11-30 20:12:27 -0500

    userfavoritegifts dto fixed....

--- GiftAPI/DTOs/UserFavoriteGiftDto.cs
+++ GiftAPI/DTOs/UserFavoriteGiftDto.cs
@@ -3,10 +3,10 @@
     public class UserFavoriteGiftDto
     {
         public int UserFavoriteGiftId { get; set; }
-        public int UserId { get; set; }
+        public int PGiftId { get; set; }
         public int GiftId { get; set; }
 
-        public UserInfoDto User { get; set; }
+        public ParentGiftsDto Parent { get; set; }
         public List<GiftInfoDto> FavoritedGifts { get; set; }
     }
 }
